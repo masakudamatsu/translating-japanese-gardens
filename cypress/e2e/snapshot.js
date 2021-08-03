466dba953db457e59d67a10adaa727397aa12d75
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-08-03 11:06:37 +0900

    test(percy): add visual regression tests for Ryoan-ji and Koho-an pages
    prevent visual regression on these two pages and test if the navigation drawer is accurately shown

--- cypress/e2e/snapshot.js
+++ cypress/e2e/snapshot.js
@@ -14,4 +14,28 @@ describe('Integration test with visual testing', () => {
     // 836px for the middle point between 728px to 945px
     // 1011px for the breakpoint to show the background on both sides
   });
+  const pages = ['ryoanji', 'kohoan'];
+  pages.forEach(page => {
+    it(`Loads ${page} page`, () => {
+      cy.visit(`/${page}`);
+      cy.scrollTo('bottom', {duration: 2000}); // Longer duration won't load more images anyway
+      cy.scrollTo('top'); // Scroll up to reveal the top app bar
+      cy.findByTestId('top-app-bar').should('be.visible');
+
+      // Snapshot without navigation drawer
+      cy.percySnapshot(page, {widths: [320, 410, 648, 836, 1011]});
+      // 320px for the narrowest screen to support
+      // 410px for font-size change breakpoint
+      // 648px for the middle point between 571px to 727px
+      // 836px for the middle point between 728px to 945px
+      // 1011px for the breakpoint to show the background on both sides
+
+      // Snapshot with navigation drawer
+      cy.findByRole('button', {name: 'Menu'}).click();
+      cy.findByTestId('nav-menu').should('be.visible');
+      cy.percySnapshot(`${page}-with-nav`, {
+        widths: [320, 410, 648, 836, 1011],
+      });
+    });
+  });
 });
